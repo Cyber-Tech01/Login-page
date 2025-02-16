794b2250602c447c03097776fdda8fe0b6db017d
Cyber -Tech <cybertech056@gmail.com>
2025-02-16 15:06:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <form action="/contact.php" method="post">
+    <input type="text" name="name" placeholder="name">
+    <input type="email" name="email" placeholder="email">
+    <input type="subject" placeholder="subject">
+    <textarea name="message" placeholder="message"></textarea>
+    <button type="submit" name="submit">Send email</button>
+
+  </form>
+</body>
+</html>
